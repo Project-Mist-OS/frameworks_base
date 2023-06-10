276f871a961af9e05356a872ead56d5424849853
Luofan Chen <clfbbn@gmail.com>
2023-06-10 11:29:07 +0330

    base: Implement Lineage health service
    Change-Id: I772ccf6d323c24d681aa8468bf4318c7b73bd3f5
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -229,6 +229,7 @@ import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 import com.android.server.mist.LineageGlobalActionsService;
 import com.android.server.mist.display.LiveDisplayService;
+import com.android.server.mist.health.HealthInterfaceService;
 
 import dalvik.system.VMRuntime;
 
@@ -2606,6 +2607,10 @@ public final class SystemServer implements Dumpable {
             // CustomDeviceConfigService
             t.traceBegin("StartCustomDeviceConfigService");
             mSystemServiceManager.startService(CustomDeviceConfigService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartHealthService");
+            mSystemServiceManager.startService(HealthInterfaceService.class);
             t.traceEnd();
         }
 
