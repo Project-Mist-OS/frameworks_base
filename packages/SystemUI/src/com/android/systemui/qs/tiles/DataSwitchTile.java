dc6feabb7c96f8c75642702351b735e641e3e752
ShukakuZa <yusufzuzu787@gmail.com>
2023-09-04 11:53:40 +0000

    fix typo error

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -240,7 +240,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.CHERISH_SETTINGS;
+        return MetricsEvent.MIST_SETTINGS;
     }
 
     /**
