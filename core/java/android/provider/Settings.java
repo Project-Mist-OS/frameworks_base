CherishOS/android_frameworks_base

Introduce Custom settings layout
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/android/provider/Settings.java
@@ -5906,6 +5906,12 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_CLOCK_SECONDS = "status_bar_clock_seconds";
+		
+		/**
+         * Settings Dashboard Style
+         * @hide
+         */
+        public static final String SETTINGS_DASHBOARD_GMS = "settings_dashboard_gms";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to

Project-Mist-OS/frameworks_base

Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

core/java/android/provider/Settings.java
@@ -5762,6 +5762,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * Settings Dashboard Style
+         * @hide
+         */
+        public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *