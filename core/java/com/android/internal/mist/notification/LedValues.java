CherishOS/android_frameworks_base

Import LineageNotification from lineage sdk
Change-Id: Ie938bfcdb735be699006b238c3a0589b6cbd9944
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/com/android/internal/cherish/notification/LedValues.java
@@ -1,0 +1,121 @@
+/**
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.cherish.notification;
+
+public class LedValues {
+    public static final int LIGHT_BRIGHTNESS_MAXIMUM = 255;
+
+    private boolean mEnabled;
+    private int mColor;
+    private int mOnMs;
+    private int mOffMs;
+    private int mBrightness;
+
+    public LedValues(int color, int onMs, int offMs) {
+        this(color, onMs, offMs, LIGHT_BRIGHTNESS_MAXIMUM);
+    }
+
+    public LedValues(int color, int onMs, int offMs, int brightness) {
+        this(true, color, onMs, offMs, brightness);
+    }
+
+    public LedValues(boolean enabled, int color, int onMs, int offMs, int brightness) {
+        mEnabled = enabled;
+        mColor = color;
+        mOnMs = onMs;
+        mOffMs = offMs;
+        mBrightness = brightness;
+    }
+
+    public boolean isEnabled() {
+        return mEnabled;
+    }
+
+    public void setEnabled(boolean enable) {
+        mEnabled = enable;
+    }
+
+    public int getColor() {
+        return mColor;
+    }
+
+    public void setColor(int color) {
+        mColor = color;
+    }
+
+    public int getOnMs() {
+        return mOnMs;
+    }
+
+    public void setOnMs(int onMs) {
+        mOnMs = onMs;
+    }
+
+    public int getOffMs() {
+        return mOffMs;
+    }
+
+    public void setOffMs(int offMs) {
+        mOffMs = offMs;
+    }
+
+    public int getBrightness() {
+        return mBrightness;
+    }
+
+    public void setBrightness(int brightness) {
+        mBrightness = brightness;
+    }
+
+    public void setSolid() {
+        mOnMs = 0;
+        mOffMs = 0;
+    }
+
+    public boolean isPulsed() {
+        return mOnMs != 0 && mOffMs != 0;
+    }
+
+    public void setPulsed(int onMs, int offMs) {
+        mOnMs = onMs;
+        mOffMs = offMs;
+    }
+
+    public void applyAlphaToBrightness() {
+        final int alpha = (mColor >> 24) & 0xFF;
+        if (alpha > 0 && alpha < 255) {
+            mBrightness = mBrightness * alpha / 255;
+        }
+        mColor |= 0xFF000000;
+    }
+
+    public void applyBrightnessToColor() {
+        if (mBrightness > 0 && mBrightness < 255) {
+            int red   = ((mColor >> 16) & 0xFF) * mBrightness / 255;
+            int green = ((mColor >>  8) & 0xFF) * mBrightness / 255;
+            int blue  = (mColor & 0xFF) * mBrightness / 255;
+            mColor = (red << 16) | (green << 8) | blue;
+            mBrightness = 255;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "enabled=" + mEnabled + " color=#" + String.format("%08X", mColor)
+                + " onMs=" + mOnMs + " offMs=" + mOffMs + " brightness=" + mBrightness;
+    }
+}

core/java/com/android/internal/cherish/notification/LightsCapabilities.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.cherish.notification;
+
+import android.content.Context;
+
+public final class LightsCapabilities {
+    // Device has a color adjustable notification light.
+    public static final int LIGHTS_RGB_NOTIFICATION_LED = 1;
+
+    // Device has a color adjustable battery light.
+    public static final int LIGHTS_RGB_BATTERY_LED = 2;
+
+    // Deprecated
+    // public static final int LIGHTS_MULTIPLE_NOTIFICATION_LED = 4;
+
+    // The notification light has adjustable pulsing capability.
+    public static final int LIGHTS_PULSATING_LED = 8;
+
+    // Device has a multi-segment battery light that is able to
+    // use the light brightness value to determine how many
+    // segments to show (in order to represent battery level).
+    public static final int LIGHTS_SEGMENTED_BATTERY_LED = 16;
+
+    // The notification light supports HAL adjustable brightness
+    // via the alpha channel.
+    // Note: if a device notification light supports LIGHTS_RGB_NOTIFICATION_LED
+    // then HAL support is not necessary for brightness control.  In this case,
+    // brightness support will be provided by lineage-sdk through the scaling of
+    // RGB color values.
+    public static final int LIGHTS_ADJUSTABLE_NOTIFICATION_LED_BRIGHTNESS = 32;
+
+    // Device has a battery light.
+    public static final int LIGHTS_BATTERY_LED = 64;
+
+    // The battery light supports HAL adjustable brightness via
+    // the alpha channel.
+    // Note: if a device battery light supports LIGHTS_RGB_BATTERY_LED then HAL
+    // support is not necessary for brightness control.  In this case,
+    // brightness support will be provided by lineage-sdk through the scaling of
+    // RGB color values.
+    public static final int LIGHTS_ADJUSTABLE_BATTERY_LED_BRIGHTNESS = 128;
+
+    public static boolean supports(Context context, final int capability) {
+        final int capabilities = context.getResources().getInteger(
+                com.android.internal.R.integer.config_deviceLightCapabilities);
+        return (capabilities & capability) != 0;
+    }
+}

core/java/com/android/internal/cherish/notification/LineageNotification.java
@@ -1,0 +1,58 @@
+/**
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.cherish.notification;
+
+public class LineageNotification {
+
+    ///////////////////////////////////////////////////
+    // Lineage-specific Notification bundle extras
+    ///////////////////////////////////////////////////
+
+    /**
+     * Used by light picker in Settings to force
+     * notification lights on when screen is on.
+     */
+    public static final String EXTRA_FORCE_SHOW_LIGHTS = "lineage.forceShowLights";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light brightness.
+     */
+    public static final String EXTRA_FORCE_LIGHT_BRIGHTNESS = "lineage.forceLightBrightness";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light color.
+     */
+    public static final String EXTRA_FORCE_COLOR = "lineage.forceColor";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light on duration.
+     *
+     * Value must be greater than or equal to 0.
+     */
+    public static final String EXTRA_FORCE_LIGHT_ON_MS = "lineage.forceLightOnMs";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light off duration.
+     *
+     * Value must be greater than or equal to 0.
+     */
+    public static final String EXTRA_FORCE_LIGHT_OFF_MS = "lineage.forceLightOffMs";
+}

Project-Mist-OS/frameworks_base

core/java/com/android/internal/mist/notification/LedValues.java
@@ -1,0 +1,121 @@
+/**
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.mist.notification;
+
+public class LedValues {
+    public static final int LIGHT_BRIGHTNESS_MAXIMUM = 255;
+
+    private boolean mEnabled;
+    private int mColor;
+    private int mOnMs;
+    private int mOffMs;
+    private int mBrightness;
+
+    public LedValues(int color, int onMs, int offMs) {
+        this(color, onMs, offMs, LIGHT_BRIGHTNESS_MAXIMUM);
+    }
+
+    public LedValues(int color, int onMs, int offMs, int brightness) {
+        this(true, color, onMs, offMs, brightness);
+    }
+
+    public LedValues(boolean enabled, int color, int onMs, int offMs, int brightness) {
+        mEnabled = enabled;
+        mColor = color;
+        mOnMs = onMs;
+        mOffMs = offMs;
+        mBrightness = brightness;
+    }
+
+    public boolean isEnabled() {
+        return mEnabled;
+    }
+
+    public void setEnabled(boolean enable) {
+        mEnabled = enable;
+    }
+
+    public int getColor() {
+        return mColor;
+    }
+
+    public void setColor(int color) {
+        mColor = color;
+    }
+
+    public int getOnMs() {
+        return mOnMs;
+    }
+
+    public void setOnMs(int onMs) {
+        mOnMs = onMs;
+    }
+
+    public int getOffMs() {
+        return mOffMs;
+    }
+
+    public void setOffMs(int offMs) {
+        mOffMs = offMs;
+    }
+
+    public int getBrightness() {
+        return mBrightness;
+    }
+
+    public void setBrightness(int brightness) {
+        mBrightness = brightness;
+    }
+
+    public void setSolid() {
+        mOnMs = 0;
+        mOffMs = 0;
+    }
+
+    public boolean isPulsed() {
+        return mOnMs != 0 && mOffMs != 0;
+    }
+
+    public void setPulsed(int onMs, int offMs) {
+        mOnMs = onMs;
+        mOffMs = offMs;
+    }
+
+    public void applyAlphaToBrightness() {
+        final int alpha = (mColor >> 24) & 0xFF;
+        if (alpha > 0 && alpha < 255) {
+            mBrightness = mBrightness * alpha / 255;
+        }
+        mColor |= 0xFF000000;
+    }
+
+    public void applyBrightnessToColor() {
+        if (mBrightness > 0 && mBrightness < 255) {
+            int red   = ((mColor >> 16) & 0xFF) * mBrightness / 255;
+            int green = ((mColor >>  8) & 0xFF) * mBrightness / 255;
+            int blue  = (mColor & 0xFF) * mBrightness / 255;
+            mColor = (red << 16) | (green << 8) | blue;
+            mBrightness = 255;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "enabled=" + mEnabled + " color=#" + String.format("%08X", mColor)
+                + " onMs=" + mOnMs + " offMs=" + mOffMs + " brightness=" + mBrightness;
+    }
+}

core/java/com/android/internal/mist/notification/LightsCapabilities.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.mist.notification;
+
+import android.content.Context;
+
+public final class LightsCapabilities {
+    // Device has a color adjustable notification light.
+    public static final int LIGHTS_RGB_NOTIFICATION_LED = 1;
+
+    // Device has a color adjustable battery light.
+    public static final int LIGHTS_RGB_BATTERY_LED = 2;
+
+    // Deprecated
+    // public static final int LIGHTS_MULTIPLE_NOTIFICATION_LED = 4;
+
+    // The notification light has adjustable pulsing capability.
+    public static final int LIGHTS_PULSATING_LED = 8;
+
+    // Device has a multi-segment battery light that is able to
+    // use the light brightness value to determine how many
+    // segments to show (in order to represent battery level).
+    public static final int LIGHTS_SEGMENTED_BATTERY_LED = 16;
+
+    // The notification light supports HAL adjustable brightness
+    // via the alpha channel.
+    // Note: if a device notification light supports LIGHTS_RGB_NOTIFICATION_LED
+    // then HAL support is not necessary for brightness control.  In this case,
+    // brightness support will be provided by lineage-sdk through the scaling of
+    // RGB color values.
+    public static final int LIGHTS_ADJUSTABLE_NOTIFICATION_LED_BRIGHTNESS = 32;
+
+    // Device has a battery light.
+    public static final int LIGHTS_BATTERY_LED = 64;
+
+    // The battery light supports HAL adjustable brightness via
+    // the alpha channel.
+    // Note: if a device battery light supports LIGHTS_RGB_BATTERY_LED then HAL
+    // support is not necessary for brightness control.  In this case,
+    // brightness support will be provided by lineage-sdk through the scaling of
+    // RGB color values.
+    public static final int LIGHTS_ADJUSTABLE_BATTERY_LED_BRIGHTNESS = 128;
+
+    public static boolean supports(Context context, final int capability) {
+        final int capabilities = context.getResources().getInteger(
+                com.android.internal.R.integer.config_deviceLightCapabilities);
+        return (capabilities & capability) != 0;
+    }
+}

core/java/com/android/internal/mist/notification/LineageNotification.java
@@ -1,0 +1,58 @@
+/**
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.mist.notification;
+
+public class LineageNotification {
+
+    ///////////////////////////////////////////////////
+    // Lineage-specific Notification bundle extras
+    ///////////////////////////////////////////////////
+
+    /**
+     * Used by light picker in Settings to force
+     * notification lights on when screen is on.
+     */
+    public static final String EXTRA_FORCE_SHOW_LIGHTS = "lineage.forceShowLights";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light brightness.
+     */
+    public static final String EXTRA_FORCE_LIGHT_BRIGHTNESS = "lineage.forceLightBrightness";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light color.
+     */
+    public static final String EXTRA_FORCE_COLOR = "lineage.forceColor";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light on duration.
+     *
+     * Value must be greater than or equal to 0.
+     */
+    public static final String EXTRA_FORCE_LIGHT_ON_MS = "lineage.forceLightOnMs";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light off duration.
+     *
+     * Value must be greater than or equal to 0.
+     */
+    public static final String EXTRA_FORCE_LIGHT_OFF_MS = "lineage.forceLightOffMs";
+}