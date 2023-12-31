CherishOS/android_frameworks_base

[SQUASH] Implement DeviceConfigUtils and service
- Allow overriding device configs while using GMS as device configurator package

Change-Id: Ie7327610feca7a390915511088dfef831c946853

DeviceConfigUtils: Make debug optional

Change-Id: I65ffac8fd0a9971297b8d721d860face571a2d44
(cherry picked from commit 9f54af5392d7ca70e1e495fc97560538459844e7)

DeviceConfig: Change setProperties behavior

Change-Id: I7cf1574f423c7362e6a7c9d8a002e4b20e5e7f87
(cherry picked from commit f94a2c97e7bf53ee6681183f2394878369e99470)

* Adapt to Android 14

Change-Id: I6ece899c13f12ee299ba24b02f2b4ab96f3d92fb
Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/com/android/internal/util/cherish/DeviceConfigUtils.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2023 The Pixel Experience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.cherish;
+
+import android.content.res.Resources;
+import android.provider.Settings;
+import android.util.Log;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import com.android.internal.util.ArrayUtils;
+
+public class DeviceConfigUtils {
+
+    private static final String TAG = DeviceConfigUtils.class.getSimpleName();
+
+    private static final boolean DEBUG = false;
+
+    private static String[] getDeviceConfigsOverride() {
+        String[] globalDeviceConfigs =
+            Resources.getSystem().getStringArray(com.android.internal.R.array.global_device_configs_override);
+        String[] deviceConfigs =
+            Resources.getSystem().getStringArray(com.android.internal.R.array.device_configs_override);
+        String[] allDeviceConfigs = Arrays.copyOf(globalDeviceConfigs, globalDeviceConfigs.length + deviceConfigs.length);
+        System.arraycopy(deviceConfigs, 0, allDeviceConfigs, globalDeviceConfigs.length, deviceConfigs.length);
+        return allDeviceConfigs;
+    }
+
+    public static boolean shouldDenyDeviceConfigControl(String namespace, String property) {
+        if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, namespace=" + namespace + ", property=" + property);
+        for (String p : getDeviceConfigsOverride()) {
+            String[] kv = p.split("=");
+            String fullKey = kv[0];
+            String[] nsKey = fullKey.split("/");
+            if (nsKey[0] == namespace && nsKey[1] == property){
+                if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, deny, namespace=" + namespace + ", property=" + property);
+                return true;
+            }
+        }
+        if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, allow, namespace=" + namespace + ", property=" + property);
+        return false;
+    }
+
+    public static void setDefaultProperties(String filterNamespace, String filterProperty) {
+        if (DEBUG) Log.d(TAG, "setDefaultProperties");
+        for (String p : getDeviceConfigsOverride()) {
+            String[] kv = p.split("=");
+            String fullKey = kv[0];
+            String[] nsKey = fullKey.split("/");
+
+            String namespace = nsKey[0];
+            String key = nsKey[1];
+
+            if (filterNamespace != null && filterNamespace == namespace){
+                continue;
+            }
+
+            if (filterProperty != null && filterProperty == key){
+                continue;
+            }
+
+            String value = "";
+            if (kv.length > 1) {
+                value = kv[1];
+            }
+            Settings.Config.putString(namespace, key, value, false);
+        }
+    }
+}

services/core/java/com/android/server/cherish/CustomDeviceConfigService.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2023 The PixelExperience Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.server.cherish;
+
+import android.content.Context;
+import com.android.server.SystemService;
+
+import com.android.internal.util.cherish.DeviceConfigUtils;
+
+public class CustomDeviceConfigService extends SystemService {
+
+    private static final String TAG = "CustomDeviceConfigService";
+
+    private final Context mContext;
+
+    public CustomDeviceConfigService(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onStart() {
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            DeviceConfigUtils.setDefaultProperties(null, null);
+        }
+    }
+}

services/java/com/android/server/SystemServer.java
@@ -132,6 +132,7 @@ import com.android.server.display.DisplayManagerService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
+import com.android.server.cherish.CustomDeviceConfigService;
 import com.android.server.gpu.GpuService;
 import com.android.server.grammaticalinflection.GrammaticalInflectionService;
 import com.android.server.graphics.fonts.FontManagerService;
@@ -2582,6 +2583,11 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartLiveDisplayService");
             mSystemServiceManager.startService(LiveDisplayService.class);
+            t.traceEnd();
+
+            // CustomDeviceConfigService
+            t.traceBegin("StartCustomDeviceConfigService");
+            mSystemServiceManager.startService(CustomDeviceConfigService.class);
             t.traceEnd();
         }
 

Project-Mist-OS/frameworks_base

core/java/com/android/internal/util/mist/DeviceConfigUtils.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2023 The Pixel Experience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.mist;
+
+import android.content.res.Resources;
+import android.provider.Settings;
+import android.util.Log;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import com.android.internal.util.ArrayUtils;
+
+public class DeviceConfigUtils {
+
+    private static final String TAG = DeviceConfigUtils.class.getSimpleName();
+
+    private static final boolean DEBUG = false;
+
+    private static String[] getDeviceConfigsOverride() {
+        String[] globalDeviceConfigs =
+            Resources.getSystem().getStringArray(com.android.internal.R.array.global_device_configs_override);
+        String[] deviceConfigs =
+            Resources.getSystem().getStringArray(com.android.internal.R.array.device_configs_override);
+        String[] allDeviceConfigs = Arrays.copyOf(globalDeviceConfigs, globalDeviceConfigs.length + deviceConfigs.length);
+        System.arraycopy(deviceConfigs, 0, allDeviceConfigs, globalDeviceConfigs.length, deviceConfigs.length);
+        return allDeviceConfigs;
+    }
+
+    public static boolean shouldDenyDeviceConfigControl(String namespace, String property) {
+        if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, namespace=" + namespace + ", property=" + property);
+        for (String p : getDeviceConfigsOverride()) {
+            String[] kv = p.split("=");
+            String fullKey = kv[0];
+            String[] nsKey = fullKey.split("/");
+            if (nsKey[0] == namespace && nsKey[1] == property){
+                if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, deny, namespace=" + namespace + ", property=" + property);
+                return true;
+            }
+        }
+        if (DEBUG) Log.d(TAG, "shouldAllowDeviceConfigControl, allow, namespace=" + namespace + ", property=" + property);
+        return false;
+    }
+
+    public static void setDefaultProperties(String filterNamespace, String filterProperty) {
+        if (DEBUG) Log.d(TAG, "setDefaultProperties");
+        for (String p : getDeviceConfigsOverride()) {
+            String[] kv = p.split("=");
+            String fullKey = kv[0];
+            String[] nsKey = fullKey.split("/");
+
+            String namespace = nsKey[0];
+            String key = nsKey[1];
+
+            if (filterNamespace != null && filterNamespace == namespace){
+                continue;
+            }
+
+            if (filterProperty != null && filterProperty == key){
+                continue;
+            }
+
+            String value = "";
+            if (kv.length > 1) {
+                value = kv[1];
+            }
+            Settings.Config.putString(namespace, key, value, false);
+        }
+    }
+}

services/core/java/com/android/server/mist/CustomDeviceConfigService.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2023 The PixelExperience Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.server.mist;
+
+import android.content.Context;
+import com.android.server.SystemService;
+
+import com.android.internal.util.mist.DeviceConfigUtils;
+
+public class CustomDeviceConfigService extends SystemService {
+
+    private static final String TAG = "CustomDeviceConfigService";
+
+    private final Context mContext;
+
+    public CustomDeviceConfigService(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onStart() {
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            DeviceConfigUtils.setDefaultProperties(null, null);
+        }
+    }
+}

services/java/com/android/server/SystemServer.java
@@ -133,6 +133,7 @@ import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
 import com.android.server.flags.FeatureFlagsService;
+import com.android.server.mist.CustomDeviceConfigService;
 import com.android.server.gpu.GpuService;
 import com.android.server.grammaticalinflection.GrammaticalInflectionService;
 import com.android.server.graphics.fonts.FontManagerService;
@@ -2591,6 +2592,11 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartLiveDisplayService");
             mSystemServiceManager.startService(LiveDisplayService.class);
+            t.traceEnd();
+
+            // CustomDeviceConfigService
+            t.traceBegin("StartCustomDeviceConfigService");
+            mSystemServiceManager.startService(CustomDeviceConfigService.class);
             t.traceEnd();
         }
 