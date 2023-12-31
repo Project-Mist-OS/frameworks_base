CherishOS/android_frameworks_base

PixelPropsUtils: Refactor
* Allows some devices to fully pass safetynet's strong attestation (maybe due to prebuilt vendor)
* Do not spoof restore, pixelmigrate and setupwizard
* Enable Photos spoof by default
* Keep Photos happy and not count towards storage quota
* Separate GamesProps into GamesPropsUtils
* Switch from POCO F4 to POCO F5
* Transfer GMS spoofing from AttestationHooks

Co-authored-by: someone5678 <nemui3353@gmail.com>
Co-authored-by: aswin7469 <aswinas@pixysos.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Gustavo Mendes <gusttavo.me@outlook.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/android/app/Instrumentation.java
@@ -19,6 +19,7 @@ package android.app;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.app.Instrumentation.ActivityResult;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
@@ -66,6 +67,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+
+import com.android.internal.util.cherish.AttestationHooks;
+import com.android.internal.util.cherish.GamesPropsUtils;
+import com.android.internal.util.cherish.PixelPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1280,6 +1285,9 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
+        AttestationHooks.setProps(context);
+        GamesPropsUtils.setProps(context);
+        PixelPropsUtils.setProps(context);
         app.attach(context);
         return app;
     }
@@ -1297,6 +1305,9 @@ public class Instrumentation {
             throws InstantiationException, IllegalAccessException, 
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
+        AttestationHooks.setProps(context);
+        GamesPropsUtils.setProps(context);
+        PixelPropsUtils.setProps(context);
         app.attach(context);
         return app;
     }

core/java/com/android/internal/util/cherish/AttestationHooks.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *           (C) 2023 ArrowOS
+ *           (C) 2023 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.cherish;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.android.internal.R;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/** @hide */
+public final class AttestationHooks {
+
+    private static final String TAG = "AttestationHooks";
+    private static final boolean DEBUG = false;
+
+    private static final String sStockFp =
+            Resources.getSystem().getString(R.string.config_stockFingerprint);
+
+    private static final String sNetflixModel =
+            Resources.getSystem().getString(R.string.config_netflixSpoofModel);
+
+    private static final String PACKAGE_ARCORE = "com.google.ar.core";
+    private static final String PACKAGE_GPHOTOS = "com.google.android.apps.photos";
+    private static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
+
+    private static final Map<String, Object> sPixelXLProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "marlin",
+        "PRODUCT", "marlin",
+        "MODEL", "Pixel XL",
+        "FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
+    );
+
+    private static final Map<String, Object> sPixel8ProProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "husky",
+        "PRODUCT", "husky",
+        "MODEL", "Pixel 8 Pro",
+        "FINGERPRINT", "google/husky/husky:14/UD1A.231105.004/11010374:user/release-keys"
+    );
+
+    private static volatile String sProcessName;
+
+    private AttestationHooks() { }
+
+    public static void setProps(Context context) {
+        final String packageName = context.getPackageName();
+        final String processName = Application.getProcessName();
+
+        if (TextUtils.isEmpty(packageName) || processName == null) {
+            return;
+        }
+
+        sProcessName = processName;
+        if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
+            dlog("Setting stock fingerprint for: " + packageName);
+            setPropValue("FINGERPRINT", sStockFp);
+        }
+
+        if (packageName.equals(PACKAGE_GPHOTOS)) {
+            if (!SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
+                dlog("Photos spoofing disabled by system prop");
+                return;
+            } else {
+                dlog("Spoofing Pixel XL for: " + packageName);
+                sPixelXLProps.forEach(AttestationHooks::setPropValue);
+            }
+        }
+
+        if (packageName.equals(PACKAGE_NETFLIX)) {
+            if (!SystemProperties.getBoolean("persist.sys.pixelprops.netflix", false)) {
+                dlog("Netflix spoofing disabled by system prop");
+                return;
+            } else if (!sNetflixModel.isEmpty() && packageName.equals(PACKAGE_NETFLIX)) {
+                dlog("Setting model to " + sNetflixModel + " for Netflix");
+                setPropValue("MODEL", sNetflixModel);
+            } else {
+                dlog("Spoofing Pixel 8 Pro for: " + packageName);
+                sPixel8ProProps.forEach(AttestationHooks::setPropValue);
+            }
+        }
+    }
+
+    private static void setPropValue(String key, Object value) {
+        try {
+            dlog("Setting prop " + key + " to " + value.toString());
+            Field field = Build.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
+
+    public static void dlog(String msg) {
+        if (DEBUG) Log.d(TAG, "[" + sProcessName + "] " + msg);
+    }
+}

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -45,6 +45,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.cherish.PixelPropsUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -174,6 +175,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        PixelPropsUtils.onEngineGetCertificateChain();
 
         KeyEntryResponse response = getKeyMetadata(alias);
 

services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -280,6 +280,8 @@ import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
 
+import com.android.internal.util.cherish.PixelPropsUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -1960,7 +1962,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3070,7 +3074,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 

Project-Mist-OS/frameworks_base

core/java/android/app/Instrumentation.java
@@ -19,6 +19,7 @@ package android.app;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.app.Instrumentation.ActivityResult;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
@@ -66,6 +67,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+
+import com.android.internal.util.mist.AttestationHooks;
+import com.android.internal.util.mist.GamesPropsUtils;
+import com.android.internal.util.mist.PixelPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1280,6 +1285,9 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
+        AttestationHooks.setProps(context);
+        GamesPropsUtils.setProps(context);
+        PixelPropsUtils.setProps(context);
         app.attach(context);
         return app;
     }
@@ -1297,6 +1305,9 @@ public class Instrumentation {
             throws InstantiationException, IllegalAccessException, 
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
+        AttestationHooks.setProps(context);
+        GamesPropsUtils.setProps(context);
+        PixelPropsUtils.setProps(context);
         app.attach(context);
         return app;
     }

core/java/com/android/internal/util/mist/AttestationHooks.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *           (C) 2023 ArrowOS
+ *           (C) 2023 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.mist;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.android.internal.R;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/** @hide */
+public final class AttestationHooks {
+
+    private static final String TAG = "AttestationHooks";
+    private static final boolean DEBUG = false;
+
+    private static final String sStockFp =
+            Resources.getSystem().getString(R.string.config_stockFingerprint);
+
+    private static final String sNetflixModel =
+            Resources.getSystem().getString(R.string.config_netflixSpoofModel);
+
+    private static final String PACKAGE_ARCORE = "com.google.ar.core";
+    private static final String PACKAGE_GPHOTOS = "com.google.android.apps.photos";
+    private static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
+
+    private static final Map<String, Object> sPixelXLProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "marlin",
+        "PRODUCT", "marlin",
+        "MODEL", "Pixel XL",
+        "FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
+    );
+
+    private static final Map<String, Object> sPixel8ProProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "husky",
+        "PRODUCT", "husky",
+        "MODEL", "Pixel 8 Pro",
+        "FINGERPRINT", "google/husky/husky:14/UD1A.231105.004/11010374:user/release-keys"
+    );
+
+    private static volatile String sProcessName;
+
+    private AttestationHooks() { }
+
+    public static void setProps(Context context) {
+        final String packageName = context.getPackageName();
+        final String processName = Application.getProcessName();
+
+        if (TextUtils.isEmpty(packageName) || processName == null) {
+            return;
+        }
+
+        sProcessName = processName;
+        if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
+            dlog("Setting stock fingerprint for: " + packageName);
+            setPropValue("FINGERPRINT", sStockFp);
+        }
+
+        if (packageName.equals(PACKAGE_GPHOTOS)) {
+            if (!SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
+                dlog("Photos spoofing disabled by system prop");
+                return;
+            } else {
+                dlog("Spoofing Pixel XL for: " + packageName);
+                sPixelXLProps.forEach(AttestationHooks::setPropValue);
+            }
+        }
+
+        if (packageName.equals(PACKAGE_NETFLIX)) {
+            if (!SystemProperties.getBoolean("persist.sys.pixelprops.netflix", false)) {
+                dlog("Netflix spoofing disabled by system prop");
+                return;
+            } else if (!sNetflixModel.isEmpty() && packageName.equals(PACKAGE_NETFLIX)) {
+                dlog("Setting model to " + sNetflixModel + " for Netflix");
+                setPropValue("MODEL", sNetflixModel);
+            } else {
+                dlog("Spoofing Pixel 8 Pro for: " + packageName);
+                sPixel8ProProps.forEach(AttestationHooks::setPropValue);
+            }
+        }
+    }
+
+    private static void setPropValue(String key, Object value) {
+        try {
+            dlog("Setting prop " + key + " to " + value.toString());
+            Field field = Build.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
+
+    public static void dlog(String msg) {
+        if (DEBUG) Log.d(TAG, "[" + sProcessName + "] " + msg);
+    }
+}

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -45,6 +45,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.mist.PixelPropsUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -174,6 +175,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        PixelPropsUtils.onEngineGetCertificateChain();
 
         KeyEntryResponse response = getKeyMetadata(alias);
 

services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -281,6 +281,8 @@ import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
 
+import com.android.internal.util.mist.PixelPropsUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -1956,7 +1958,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3065,7 +3069,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 